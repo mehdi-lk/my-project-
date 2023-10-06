8571822e436ea76fc666a6d141654fb9b5f8bfe4
mehdi-lk <114786618+mehdi-lk@users.noreply.github.com>
2023-10-06 19:46:54 -0400

    b

--- ex2.java
+++ ex2.java
@@ -13,3 +13,37 @@ public class TAssert {
         int assertCount = calculateAssertionCount(fileName);
         System.out.println("Number of Assertions: " + assertCount);
     }
+        private static int calculateAssertionCount(String fileName) {
+        int assertCount = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+               
+                line = line.trim();
+
+                if (line.contains("Assert.") && line.contains("(") && line.contains(")")) {
+                    assertCount += countAssertionsInLine(line);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println( e.getMessage());
+            System.exit(1);
+        }
+      return assertCount;
+    }
+
+    private static int countAssertionsInLine(String line) {
+        int count = 0;
+
+       
+        String[] parts = line.split("\\(");
+        for (String part : parts) {
+            if (part.contains("Assert.")  {
+                count++;
+            }
+        }
+
+        return count;
+    }
